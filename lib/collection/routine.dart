b870c9388aadc00e8c08bae7026f1a9ffeede925
mmohammadmfallahm02 <mmohammadmfallahm02@gmail.com>
2023-12-08 22:54:44 +0330

    add collections

---
+++ lib/collection/routine.dart
@@ -0,0 +1,20 @@
+import 'package:daily_routine_app_isar/collection/category.dart';
+import 'package:isar/isar.dart';
+
+part 'routine.g.dart';
+
+@Collection()
+class Routine {
+  Id id = Isar.autoIncrement;
+
+  late String title;
+
+  @Index()
+  late DateTime startTime;
+
+  @Index(caseSensitive: false)
+  late String day;
+
+  @Index(composite: [CompositeIndex('title')])
+  final category = IsarLink<Category>();
+}
